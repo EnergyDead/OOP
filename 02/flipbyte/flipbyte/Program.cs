3928d1842ad7ac012d46c7aa16601ce729bd99af
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2021-03-04 19:42:16 +0300

    Изменил логигу перемещения байта

--- 02/flipbyte/flipbyte/Program.cs
+++ 02/flipbyte/flipbyte/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 
 namespace flipbyte
 {
     class Program
     {
-        static int Main( string[] args )
+        public static int Main( string[] args )
         {
             int number;
             try
@@ -21,40 +19,34 @@ namespace flipbyte
                 return 1;
             }
 
-            byte[] bytArray = BitConverter.GetBytes( number );
-            BitArray bit = new BitArray( bytArray );
-            int[] result = new int[1];
-            ReversBit( bit );
-            bit.CopyTo( result, 0 );
+            byte result = FlipByte( number );
 
-            Console.WriteLine( result[0] );
-            File.WriteAllText( args[1], result[0].ToString() );
+            Console.WriteLine( result );
+            File.WriteAllText( args[1], result.ToString() );
 
             return 0;
         }
 
-        static int IsCorrectArgument( string args )
+        private static byte FlipByte( int number )
+        {
+            byte revert = (byte)number;
+            byte result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result <<= 1;
+                result |= (byte)(revert & 1);
+                revert >>= 1;
+            }
+
+            return result;
+        }
+
+        private static int IsCorrectArgument( string args )
         {
             int number = Convert.ToInt32( args );
             if ( ( 255 < number ) || ( number < 0 )  ) 
             {
                 throw new Exception( "Argument is not in 0..255" );
-            }
-
-            return number;
-        }
-
-        static BitArray ReversBit( BitArray number )
-        {
-            bool[] tempArray = new bool[8];
-            for ( int i = 0; i < 8; i++)
-            {
-                tempArray[i] = number[i];
-            }
-            Array.Reverse(tempArray);
-            for (int i = 0; i < 8; i++)
-            {
-                number[i] = tempArray[i];
             }
 
             return number;
