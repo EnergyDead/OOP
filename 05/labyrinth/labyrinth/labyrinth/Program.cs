142f2c0762afcbebe9ac0ec9528578a8060723c4
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2021-03-04 19:53:06 +0300

    Добавил 5 задачу

---
+++ 05/labyrinth/labyrinth/labyrinth/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace labyrinth
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return 1;
+        }
+    }
+}
