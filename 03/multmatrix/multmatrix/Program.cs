0f5695a1ee8536e2c87e2c17000d8462dd7f9ec3
Ruslan <rusvas012@gmail.com>
2021-03-14 11:33:07 +0300

    Broocked into functions Multimatrix

--- 03/multmatrix/multmatrix/Program.cs
+++ 03/multmatrix/multmatrix/Program.cs
@@ -7,9 +7,15 @@ namespace multmatrix
 {
     class Program
     {
+        private struct Multiply
+        {
+            public double[][] firstMatrix;
+            public double[][] secondMatrix;            
+        }
+
         static int Main(string[] args)
         {
-            if (args.Length != 2)
+            if ( args.Length != 3 )
             {
                 Console.WriteLine( "Invalid arguments count" );
                 Console.WriteLine( "Uasge: multmatrix.exe <matrix file1> <matrix file2>" );    
@@ -17,16 +23,12 @@ namespace multmatrix
                 return 1;
             }
 
-            string firstTerm = File.ReadAllText( args[0] );
-            string secondTerm = File.ReadAllText( args[1] );
+            Multiply multiplyArg;
 
-            //read matrix in input file
-            double[][] firstMatrix;
-            double[][] secondMatrix;
             try
             {
-                firstMatrix = ReadMatrix( firstTerm );
-                secondMatrix = ReadMatrix( secondTerm );
+                multiplyArg.firstMatrix = ReadMatrix( args[0] );
+                multiplyArg.secondMatrix = ReadMatrix( args[1] );
             }
             catch ( Exception error )
             {
@@ -35,36 +37,20 @@ namespace multmatrix
                 return 1;
             }
 
-            double[][] resultMatrix = new double[3][];
-            for ( int i = 0; i < 3; ++i )
-            {
-                resultMatrix[i] = new double[3];
-            }
+            string resault = MultiplyMatrix( multiplyArg );
 
-            Parallel.For(0, firstMatrix.Length, i =>
-            {
-                for (int j = 0; j < 3; ++j)
-                {
-                    for (int k = 0; k < 3; ++k)
-                    {
-                        resultMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
-                    }
-                }
-            });
-
-            Console.WriteLine( MatrixAsString( resultMatrix ) );
+            Console.WriteLine( resault );
+            File.WriteAllText( args[2], resault );
 
             return 0;
         }
 
-        static double[][] ReadMatrix( string matrix )
+        static double[][] ReadMatrix( string pathToMatrix )
         {
+            string matrix = File.ReadAllText( pathToMatrix );
             double[][] resultMatrix = new double[3][];
             int i = 0;
-            while ( matrix.Contains( "  " ) )
-            {
-                matrix = matrix.Replace( "  ", " " );
-            }
+            matrix = DeleteSpasceInMatrix( matrix );
             foreach ( string row in matrix.Trim().Split( '\n' ) )
             {
                 resultMatrix[i] = row.Trim().Split( ' ' ).Select( Convert.ToDouble ).ToArray();
@@ -74,6 +60,28 @@ namespace multmatrix
             return resultMatrix;
         }
 
+        static string MultiplyMatrix( Multiply multiplyArg )
+        {
+            double[][] resultMatrix = new double[3][];
+            for (int i = 0; i < 3; ++i)
+            {
+                resultMatrix[i] = new double[3];
+            }
+
+            Parallel.For(0, multiplyArg.firstMatrix.Length, i =>
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    for (int k = 0; k < 3; ++k)
+                    {
+                        resultMatrix[i][j] += multiplyArg.firstMatrix[i][k] * multiplyArg.secondMatrix[k][j];
+                    }
+                }
+            });
+
+            return MatrixAsString(resultMatrix);
+        }
+
         static string MatrixAsString( double[][] matrix )
         {
             string result = String.Empty;
@@ -81,12 +89,22 @@ namespace multmatrix
             {
                 for ( int j = 0; j < matrix[ i ].Length; ++j )
                 {
-                    result += matrix[i][j].ToString("F3").PadLeft(8) + " ";
+                    result += matrix[i][j].ToString( "F3" ).PadLeft(8) + " ";
                 }
                 result += Environment.NewLine;
             }
 
             return result;
+        }
+
+        static string DeleteSpasceInMatrix( string matrix )
+        {            
+            while ( matrix.Contains( "  " ) )
+            {
+                matrix = matrix.Replace( "  ", " " );
+            }
+
+            return matrix;
         }
     }
 }
--- 03/multmatrix/multmatrix/Tests.bat
+++ 03/multmatrix/multmatrix/Tests.bat
@@ -13,6 +13,24 @@ REM If non send argument, program must fail
 %MyProgram% && goto err
 echo Test 1 passed
 
+REM Successfully multyply matrices
+%MyProgram% "%~dp0first_0.txt" "%~dp0second_0.txt" "%TEMP%\output.txt" || goto err
+fc "%TEMP%\output.txt" "%~dp0result_0.txt" || goto err
+echo Test 2 passed
+
+REM Successfully multyply matrices
+%MyProgram% "%~dp0first_1.txt" "%~dp0second_1.txt" "%TEMP%\output.txt" || goto err
+fc "%TEMP%\output.txt" "%~dp0result_1.txt" || goto err
+echo Test 3 passed
+
+REM If argument not secoed file, program must fail
+%MyProgram% "%~dp0first_0.txt"  && goto err
+echo Test 4 passed
+
+REM If argument not temp file, program must fail
+%MyProgram% "%~dp0first_1.txt" "%~dp0second_1.txt"  && goto err
+echo Test 5 passed
+
 REM All tests passed successfully
 echo All tests passed successfully
 exit /B 0
