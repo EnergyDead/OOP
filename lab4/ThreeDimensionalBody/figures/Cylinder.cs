58c5d1b37cc934935d8e3905fc99edfb5ca25955
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2021-09-30 17:21:15 +0300

    lw4 добавил цилиндр

---
+++ lab4/ThreeDimensionalBody/figures/Cylinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ThreeDimensionalBody.figures
+{
+    public class Cylinder : Body
+    {
+        private double _height;
+        private double _radius;
+
+        public Cylinder( double height, double radius, double density )
+        {
+            SetRadius( radius );
+            SetDensity( density );
+            SetHeight( height );
+            SetVolume();
+            SetMass();
+        }
+
+        public double GetBaseRadius()
+        {
+            return _radius;
+        }
+
+        public double GetHeight()
+        {
+            return _height;
+        }
+
+        public override string ToString()
+        {
+            return $"Цилиндр\nМасса: {GetMass()}\nОбъем: {GetVolume()}\nПлотность: {GetDensity()}";
+        }
+
+        protected override void SetVolume()
+        {
+            _volume = Math.PI * Math.Pow( GetBaseRadius(), 2 ) * GetHeight();
+        }
+
+        private void SetHeight( double height )
+        {
+            _height = height;
+        }
+
+        private void SetRadius( double radius )
+        {
+            _radius = radius;
+        }
+    }
+}
---
+++ lab4/ThreeDimensionalBodyTests/CylinderTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreeDimensionalBody.figures;
+
+namespace ThreeDimensionalBodyTests
+{
+    [TestClass]
+    public class CylinderTests
+    {
+        [TestMethod]
+        public void CreateCylinder_CorrectArguments_Create()
+        {
+            var body = new Cylinder( 10, 10, 10 );
+
+            double baseRadius = body.GetBaseRadius();
+            double height = body.GetHeight();
+            string info = body.ToString();
+
+            Assert.AreEqual( 10, baseRadius );
+            Assert.AreEqual( 10, height );
+            Assert.AreEqual( "Цилиндр\nМасса: 31415.926535897932\nОбъем: 3141.5926535897934\nПлотность: 10", info );
+        }
+    }
+}
