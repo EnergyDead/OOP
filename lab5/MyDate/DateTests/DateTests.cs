53b6cd6d95a735d35777c0c3a006238cbf6fb3e7
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2021-10-06 20:52:07 +0300

    lw5 добавил операторы

--- lab5/MyDate/DateTests/DateTests.cs
+++ lab5/MyDate/DateTests/DateTests.cs
@@ -12,30 +12,119 @@ namespace DateTests
             Date date = new( 12, (Month)1, 2000 );
 
             Assert.NotNull( date );
+            Assert.True( date.IsValid() );
             Assert.Equal( 12, date.GetDay() );
             Assert.Equal( Month.JANUARY, date.GetMonth() );
             Assert.Equal( 2000, date.GetYear() );
         }
 
         [Fact]
-        public void CreateDate_EmptyInput_CorrectCreate()
+        public void CreateDate_ZeroInput_CorrectCreate()
         {
             Date date = new( 0 );
+            Date date1 = new();
 
             Assert.NotNull( date );
+            Assert.True( date.IsValid() );
+
             Assert.Equal( 1, date.GetDay() );
             Assert.Equal( Month.JANUARY, date.GetMonth() );
             Assert.Equal( 1970, date.GetYear() );
         }
 
         [Fact]
-        public void CreateDate_InccorectyInput_FailedTrue()
+        public void CreateDate_BigInput_CorrectCreate()
         {
-            Date date = new( 0 );
+            Date date = new( 20000 );
 
             Assert.NotNull( date );
+            Assert.True( date.IsValid() );
+
+            Assert.Equal( 3, date.GetDay() );
+            Assert.Equal( Month.OCTOBER, date.GetMonth() );
+            Assert.Equal( 2024, date.GetYear() );
+        }
+
+        [Fact]
+        public void CreateDate_IncorrectInput_FailedTrue()
+        {
+            Date date = new( 20000000 );
+
+            Assert.NotNull( date );
+            Assert.False( date.IsValid() );
+        }
+
+        [Fact]
+        public void CheckOperandPlasOnDay_CorrectDate()
+        {
+            // Arrange
+            Date date = new( 30 );
+            int oldDay = date.GetDay();
+            var oldMonth = date.GetMonth();
+            int oldYear = date.GetYear();
+
+            // Act
+            date++;
+
+            // Assert
+            Assert.NotNull( date );
+            Assert.True( date.IsValid() );
+
             Assert.Equal( 1, date.GetDay() );
-            Assert.Equal( Month.JANUARY, date.GetMonth() );
+            Assert.Equal( 30, oldDay );
+
+            Assert.Equal( Month.JANUARY, oldMonth );
+            Assert.Equal( Month.FEBRUARY, date.GetMonth() );
+            Assert.Equal( oldYear, date.GetYear() );
+        }
+
+        [Fact]
+        public void CheckOperandMinusOnDay_CorrectDate()
+        {
+            // Arrange
+            Date date = new( 366 );
+            int oldDay = date.GetDay();
+            var oldMonth = date.GetMonth();
+            int oldYear = date.GetYear();
+
+            // Act
+            date--;
+
+            // Assert
+            Assert.NotNull( date );
+            Assert.True( date.IsValid() );
+
+            Assert.Equal( 31, date.GetDay() );
+            Assert.Equal( 1, oldDay );
+
+            Assert.Equal( Month.JANUARY, oldMonth );
+            Assert.Equal( Month.DECEMBER, date.GetMonth() );
+            Assert.Equal( 1971, oldYear );
+            Assert.Equal( 1970, date.GetYear() );
+        }
+
+        [Fact]
+        public void CheckOperandMinusMoreDay_CorrectDate()
+        {
+            // Arrange
+            Date date = new( 366 );
+            int oldDay = date.GetDay();
+            var oldMonth = date.GetMonth();
+            int oldYear = date.GetYear();
+
+            // Act
+            date -= 12;
+
+            // Assert
+            Assert.NotNull( date );
+            Assert.True( date.IsValid() );
+
+            Assert.Equal( 20, date.GetDay() );
+            Assert.Equal( 1, oldDay );
+
+            Assert.Equal( Month.JANUARY, oldMonth );
+            Assert.Equal( Month.DECEMBER, date.GetMonth() );
+            Assert.Equal( 1971, oldYear );
             Assert.Equal( 1970, date.GetYear() );
         }
     }
--- lab5/MyDate/MyDate/Date.cs
+++ lab5/MyDate/MyDate/Date.cs
@@ -4,50 +4,49 @@ namespace MyDate
 {
     public class Date
     {
-        private readonly int MIN_YEAR = 1970;
-        private readonly int MIN_MONTH = 1;
-        private readonly int MAX_MONTH = 12;
-        private readonly int MIN_DAY = 1;
+        private static readonly ushort MIN_DAY = 1;
+        private static readonly ushort MIN_MONTH = 1;
+        private readonly ushort MAX_MONTH = 12;
+        private static readonly ushort MIN_YEAR = 1970;
 
-        private int _day;
-        private Month _month;
-        private int _year;
-        private bool _isValid;
+        private ushort _day;
+        private Month _month = (Month)MIN_MONTH;
+        private ushort _year = MIN_YEAR;
+        private bool _isValid = true;
 
 
         public Date( int day = 0 )
         {
-            _day = MIN_DAY;
-            _month = (Month)MIN_MONTH;
-            _year = MIN_YEAR;
+            if ( day == 0 )
+            {
+                day++;
+            }
 
             AddDays( day );
         }
 
         public Date( int day, Month month, int year )
         {
-            _isValid = true;
-
             if ( year < MIN_YEAR )
             {
                 _isValid = false;
             }
 
-            if ( MIN_MONTH > (int)month || MAX_MONTH < (int)month )
+            if ( (ushort)month < MIN_MONTH || (ushort)month > MAX_MONTH )
             {
                 _isValid = false;
             }
 
-            if ( MIN_DAY > day || DayInMonth( month ) < day )
+            if ( day < MIN_DAY || day > DayInMonth( month ) )
             {
                 _isValid = false;
             }
 
             if ( IsValid() )
             {
-                _day = day;
+                _day = (ushort)day;
                 _month = month;
-                _year = year;
+                _year = (ushort)year;
             }
         }
 
@@ -75,28 +74,96 @@ namespace MyDate
             return _isValid;
         }
 
-        public static Date operator +( Date ob, int days )
+        public static Date operator +( Date oldDate, int days )
         {
-            Date result = new Date( ob._day, ob._month, ob._year );
-            result.AddDays( days );
+            Date newDate = new Date( oldDate._day, oldDate._month, oldDate._year );
+            newDate.AddDays( days );
 
-            return result;
+            return newDate;
         }
 
-        public void AddDays( int days )
+        public static Date operator +( Date date, ushort day )
         {
-            _day += days;
-            int dayInCurrentMonth;
-            while ( _day > ( dayInCurrentMonth = DayInMonth( _month ) ) )
+            date.AddDays( day );
+
+            return date;
+        }
+
+        public static Date operator ++( Date date )
+        {
+            date.AddDays( 1 );
+
+            return date;
+        }
+
+        public static Date operator -( Date date, ushort day )
+        {
+            date.MinusDays( day );
+
+            return date;
+        }
+
+        public static Date operator --( Date date )
+        {
+            date.MinusDays( 1 );
+
+            return date;
+        }
+
+        private void MinusDays( ushort downDay )
+        {
+            int day = _day;
+            day -= downDay;
+            while ( day < 1 )
             {
-                _day -= dayInCurrentMonth;
-
-                if ( (int)++_month > 12 )
+                if ( (int)--_month < 1 )
                 {
-                    _month = Month.JANUARY;
+                    _month = Month.DECEMBER;
+                    _year--;
 
-                    _year++;
+                    if ( _year < MIN_YEAR )
+                    {
+                        _isValid = false;
+                    }
                 }
+
+                day += DayInMonth( _month );
+            }
+
+            _day = (ushort)day;
+        }
+
+        private void AddDays( int days )
+        {
+            days += _day;
+            int dayInCurrentMonth;
+            while ( days > ( dayInCurrentMonth = DayInMonth( _month ) ) )
+            {
+                days -= dayInCurrentMonth;
+                CheckMonth();
+
+            }
+
+            _day = (ushort)days;
+        }
+
+        private void CheckMonth()
+        {
+            if ( (int)++_month > 12 )
+            {
+                _month = Month.JANUARY;
+
+                CheckYear();
+            }
+        }
+
+        private void CheckYear()
+        {
+            _year++;
+
+            if ( _year > 9999 )
+            {
+                _isValid = false;
             }
         }
 
