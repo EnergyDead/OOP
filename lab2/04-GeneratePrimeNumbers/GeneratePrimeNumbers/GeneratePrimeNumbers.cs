f4ca9aa3376d263807ab7b7e05cff061e8103c34
Ruslan <rusvas012@gamil.com>
2022-03-14 00:07:42 +0300

    2.4 Генератор простых чисел

---
+++ lab2/04-GeneratePrimeNumbers/GeneratePrimeNumbers/GeneratePrimeNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneratePrimeNumbers
+{
+    public static class GeneratePrimeNumbers
+    {
+        private const int MaxBound = 100000000; 
+        public static HashSet<int> GeneratePrimeNumbersSet( int upperBound )
+        {
+            if ( upperBound < 2 || upperBound > MaxBound )
+            {
+                throw new ArgumentOutOfRangeException( $"Use a number between 2 and {MaxBound}." );
+            }
+            int max = (int)Math.Sqrt( upperBound ) + 1;
+            var result = new HashSet<int>();
+
+            var primes = Enumerable.Repeat( true, upperBound + 1 ).ToArray();
+            for ( int i = 2; i <= max; i +=2 )
+            {
+                if ( primes[ i ] )
+                {
+                    for ( int j = i * 2; j < primes.Length; j += i )
+                    {
+                        primes[ j ] = false;
+                    }
+                } 
+                if ( i == 2 ) i--;
+            }
+            for ( int i = 2; i < primes.Length; i++ )
+            {
+                if ( primes[ i ] )
+                {
+                    result.Add( i );
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lab2/04-GeneratePrimeNumbers/GeneratePrimeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneratePrimeNumbers
+{
+    public class Program
+    {
+        static void Main()
+        {
+            if ( !int.TryParse( Console.ReadLine(), out int number ) )
+            {
+                Console.WriteLine( "Error convert input lint to number. Please write integer." );
+                return;
+            }
+
+            HashSet<int> result;
+            try
+            {
+                result = GeneratePrimeNumbers.GeneratePrimeNumbersSet( number );
+            }
+            catch ( Exception error )
+            {
+                Console.WriteLine( error.Message );
+                return;
+            }
+
+            Console.WriteLine( string.Join( ", ", result ) );
+        }
+    }
+}
