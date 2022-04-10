ee92904e4ff44a6dc921a51b1a8375b50ac9429f
Ruslan <rusvas012@gmail.com>
2022-04-11 01:50:35 +0300

    5.1 Rational

---
+++ lab5/MyRational/MyRational/Rational.cs
@@ -0,0 +1,121 @@
+﻿namespace MyRational;
+
+public class Rational
+{
+    public int Numerator { get; private set; }
+    public int Denominator { get; private set; }
+    public double Value => (double) Numerator /  Denominator;
+    public Rational()
+    {
+        Numerator = 0;
+        Denominator = 1;
+    }
+
+    // Конструирует рациональное число, равное value (value/1)
+    public Rational( int value )
+    {
+        Numerator = value;
+        Denominator = 1;
+    }
+    // Конструирует рациональное число, равное numerator/denominator
+    // Рациональное число должно храниться в нормализованном виде:
+    //	знаменатель положительный (числитель может быть отрицательным)
+    //	числитель и знаменатель не имеют общиз делителей (6/8 => 3/4 и т.п.)
+    // Если знаменатель равен нулю, должно сконструироваться рациональное число, равное нулю,
+    // либо должно быть выброшено исключение std::invalid_argument.
+    public Rational( int numerator, int denominator )
+    {
+        Numerator = numerator;
+        if ( denominator <= 0 )
+        {
+            throw new ArgumentException();
+        }
+        Denominator = denominator;
+    }
+
+    // Возвращает числитель
+    public int GetNumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Возвращает знаменатель (натуральное число)
+    public int GetDenominator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Возвращает отношение числителя и знаменателя в виде числа double
+    public double ToDouble()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Прочие операторы согласно заданию
+
+}
+
+#if false
+public struct RationalNumber
+{
+    public int Numerator { get; private set; }
+    public uint Denominator { get; private set; }
+    public double Rational => ( (double) Numerator ) / ( (double) Denominator );
+    public RationalNumber( int Numerator, uint Denominator )
+    {
+        uint gcd = 1;
+        if ( Numerator == 0 )
+        {
+            this.Numerator = 0;
+            this.Denominator = 1;
+            return;
+        }
+        if ( Numerator > 0 )
+            gcd = GCD( (uint) Numerator, Denominator );
+        else
+            gcd = GCD( (uint) -Numerator, Denominator );
+        this.Numerator = Numerator / ( (int) gcd );
+        this.Denominator = Denominator / gcd;
+    }
+
+    public static implicit operator double( RationalNumber r1 ) => r1.Rational;
+    public static implicit operator RationalNumber( int r1 ) => new RationalNumber( r1, 1 );
+
+    public static RationalNumber operator +( RationalNumber r1, RationalNumber r2 )
+    {
+        return new RationalNumber
+            (
+                (int) ( r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator ),
+                r1.Denominator * r2.Denominator
+            );
+    }
+    public static RationalNumber operator *( RationalNumber r1, RationalNumber r2 )
+    {
+        return new RationalNumber
+            (
+                (int) ( r1.Numerator * r2.Numerator ),
+                 (uint) ( r1.Denominator * r2.Denominator )
+            );
+    }
+    public static RationalNumber operator -( RationalNumber r1, RationalNumber r2 )
+    {
+        return r1 + ( -1 * r2 );
+    }
+    public static RationalNumber operator /( RationalNumber r1, RationalNumber r2 )
+    {
+        return r1 * r2.Numerator < 0
+            ? new RationalNumber( (int) -r2.Denominator, (uint) -r2.Numerator )
+            : new RationalNumber( (int) r2.Denominator, (uint) r2.Numerator );
+    }
+
+    public static uint GCD( uint A, uint B )
+    {
+        uint C = A;
+        A = A > B ? A : B;
+        B = C > B ? B : C;
+        C = A % B;
+        if ( C == 0 ) return B;
+        return GCD( B, C );
+    }
+}
+#endif
