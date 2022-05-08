7f5a4442944dbb76974b089a73581d3c924080d4
Ruslan <rusvas012@gmail.com>
2022-05-09 02:11:49 +0300

    6.2 StringList - init

---
+++ lab6/MyStringList/MyStringList/StringList.cs
@@ -0,0 +1,8 @@
+﻿namespace MyStringList;
+
+public class StringList
+{
+    private StringNode _first;
+    private StringNode _last;
+
+}
---
+++ lab6/MyStringList/MyStringList/StringNode.cs
@@ -0,0 +1,13 @@
+﻿namespace MyStringList;
+
+internal class StringNode
+{
+    private string _value;
+    public string Value { get => _value; set => _value = value; }
+
+    private StringNode? _next;
+    public StringNode? Next { get => _next; set => _next = value; }
+
+    private StringNode? _previous;
+    public StringNode? Previous { get => _previous; set => _previous = value; }
+}
