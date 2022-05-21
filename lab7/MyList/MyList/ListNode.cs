70e3d4adc9a6345eb8b1b7037ea4f9ffab01db1e
Ruslan <rusvas012@gmail.com>
2022-05-21 19:14:41 +0300

    7.3 MyList - init

---
+++ lab7/MyList/MyList/ListNode.cs
@@ -0,0 +1,26 @@
+﻿namespace MyList;
+
+public class ListNode<T>
+{
+    internal MyList<T> _list;
+    internal ListNode<T> _next;
+    internal ListNode<T> _prev;
+    internal T _value;
+
+    public T Value { get { return _value; } }
+
+    // todo: сделать проверку на head
+    public ListNode<T> Next { get { return _next == null || _next == _list._head ? null : _next; } }
+    public ListNode<T> Prev { get { return _prev == null || this == _list._head ? null : _prev; } }
+
+    public ListNode(T value)
+    {
+        _value = value;
+    }
+
+    internal ListNode(MyList<T> list, T value)
+    {
+        _list = list;
+        _value = value;
+    }
+}
---
+++ lab7/MyList/MyList/MyList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace MyList;
+
+public class MyList<T> : IEnumerable<T>
+{
+    internal ListNode<T> _head = null;
+    private int _count = 0;
+
+    public ListNode<T> this[int index]
+    {
+        get
+        {
+            if (index < 0 || index > _count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            var curr = _head;
+            for (int i = 0; i < index; i++)
+            {
+                curr = curr.Next;
+            }
+            return curr;
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        var curr = _head;
+        while (curr != null)
+        {
+            yield return curr;
+            curr = curr.Next;
+        }
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        return (IEnumerator<T>)GetEnumerator();
+    }
+}
