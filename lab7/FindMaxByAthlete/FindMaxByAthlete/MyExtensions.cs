b049a267618f624cb1c16e8f4859291301f8804a
Ruslan <rusvas012@gmail.com>
2022-05-23 02:26:05 +0300

    7.1 FindMax

---
+++ lab7/FindMaxByAthlete/FindMaxByAthlete/MyExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtensionMethods;
+
+internal static class MyExtensions
+{
+    public static bool FindMaxEx<T>(this IEnumerable<T> arr, ref T maxV)
+    {
+        if (!arr.Any())
+        {
+            return false;
+        }
+
+        throw new NotImplementedException();
+    }
+}
---
+++ lab7/FindMaxByAthlete/FindMaxByAthlete/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethods;
+
+Console.WriteLine("Hello, World!");
+
+var studName = new string[] { "Bob", "Max", "Julli" };
+string max = string.Empty;
+studName.FindMaxEx(ref max);
+Console.WriteLine(max);
